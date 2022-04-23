58f2f5706973f6f7e2c2119fc63854304e8db16c
YHTR0257 <grandflight@outlook.com>
2022-04-23 17:15:31 +0900

    Fix problems of not using bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,13 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import 'bootstrap';
-import '../stylesheets/application';
 import "channels"
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
