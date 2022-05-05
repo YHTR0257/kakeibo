3427d2b4812caa1019ce97a326a2812bdbf6b7a6
YHTR0257 <grandflight@outlook.com>
2022-05-05 11:16:14 +0900

    Fix conflict stash

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_121303) do
+ActiveRecord::Schema.define(version: 2022_05_05_001110) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "posts", force: :cascade do |t|
